33f174777e24cf05594460a82ae2dd68380a87be
SAK74 <sergijks@gmail.com>
2023-06-17 22:56:59 +0200

    feat: complete streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,9 @@
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream("src/streams/files/fileToRead.txt", "utf-8");
+  stream.pipe(stdout);
 };
 
 await read();

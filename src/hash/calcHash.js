468dffe0039a284bc159670d4936411294ca3a5f
SAK74 <sergijks@gmail.com>
2023-06-17 11:20:25 +0200

    feat: complete modules & hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,10 @@
+import { createHash } from "node:crypto";
+import { readFile } from "node:fs/promises";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("SHA256");
+  hash.update(await readFile("src/hash/files/fileToCalculateHashFor.txt"));
+  console.log(hash.digest("hex"));
 };
 
 await calculateHash();

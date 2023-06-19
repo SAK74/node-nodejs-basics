30519f3ebf7d29428711d4676b1e353979fd1ebb
SAK74 <sergijks@gmail.com>
2023-06-19 18:31:11 +0200

    feat: child_process complete

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,11 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from "node:child_process";
+
+const spawnChildProcess = async (...args) => {
+  const child = fork("src/cp/files/script", args, {});
+  child.on("exit", (code) => {
+    console.log("Child process has exited with code ", code);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(1, 2, 3);
